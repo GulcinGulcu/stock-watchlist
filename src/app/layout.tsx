3021f3aaae599090001345615605a66efe9aa4ba
gulcin <gulcin_375@hotmail.com>
2026-03-12 12:26:44 +0100

    chore: update favicon and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Financial Data View",
+  title: "Stock Watchlist",
   description: "Track stock information by symbol",
 };
 
